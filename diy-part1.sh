54ac2b5faca9a9f10535fc28c528633bbc1cb680
mmc1987 <49101709+mmc1987@users.noreply.github.com>
2025-04-06 07:13:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git zerotier https://github.com/mwarning/zerotier-openwrt.git' >>feeds.conf.default
+# echo 'src-git zerotier https://github.com/mwarning/zerotier-openwrt.git' >>feeds.conf.default
 
 # git clone https://github.com/asvow/luci-app-tailscale feeds/luci/applications/luci-app-tailscale
 
